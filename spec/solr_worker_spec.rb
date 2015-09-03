79c5b6027799e989453c981f43aacfe8905cd9b7
Ilya Anisimoff <ilya@intersect.org.au>
2015-09-03 12:15:37 +1000

    HCSVLAB-1063: Adds inital SolrWorker tests

--- spec/solr_worker_spec.rb
+++ spec/solr_worker_spec.rb
@@ -18,6 +18,14 @@ describe SolrWorker do
       solr_worker.subscribe
     end
 
+    it 'should parse json messages' do
+      message = 'Message 1'
+      exchange = solr_worker.get_exchange
+      expect(solr_worker).to receive(:on_message).with(message)
+      exchange.publish(message)
+      solr_worker.subscribe
+    end
+
   end
 
   
