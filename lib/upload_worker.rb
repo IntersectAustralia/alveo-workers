d505c0b2fcea2421b737f2d45d98bf716752b388
Ilya Anisimoff <anisimoff@gmail.com>
2015-09-25 11:36:08 +1000

    HCSVLAB-1065: Adds postgres message generation to upload worker

--- lib/upload_worker.rb
+++ lib/upload_worker.rb
@@ -6,9 +6,11 @@ class UploadWorker < Worker
 
   include MetadataHelper
   include SolrHelper
+  include PostgresHelper
 
   def initialize(options)
     @solr_queue_name = options[:solr_queue]
+    @postgres_queue_name = options[:postgres_queue]
     super(options[:rabbitmq])
     set_solr_config(options[:solr])
   end
@@ -16,9 +18,11 @@ class UploadWorker < Worker
   def connect
     super
     @solr_queue = add_queue(@solr_queue_name)
+    @postgres_queue= add_queue(@postgres_queue_name)
   end
 
   def process_message(message)
+    # TODO change these to CRUD verbs
     if message['action'] = 'add item'
       add_item(message['metadata'])
     end
@@ -26,11 +30,22 @@ class UploadWorker < Worker
 
   def add_item(metadata)
     expanded_json_ld = expand_json_ld(metadata)
+    create_item_solr(expanded_json_ld)
+    create_item_postgres(expanded_json_ld)
+  end
+
+  def create_item_solr(expanded_json_ld)
     solr_document = create_solr_document(expanded_json_ld)
     # TODO: Move action type to message header
     # TODO: extract to message builder utility class
     message = "{\"action\": \"add\", \"document\": #{solr_document.to_json}}"
     @exchange.publish(message, routing_key: @solr_queue.name)
+  end
+
+  def create_item_postgres(expanded_json_ld)
+    postgres_data = create_pg_statement(expanded_json_ld)
+    message = "{\"action\": \"create\", \"payload\": #{postgres_data.to_json}}"
+    @exchange.publish(message, routing_key: @postgres_queue.name)
   end
 
 end
--- spec/upload_worker_spec.rb
+++ spec/upload_worker_spec.rb
@@ -4,22 +4,36 @@ describe UploadWorker do
 
   before(:all) do
     @options = {rabbitmq: {client_class: 'BunnyMock', work_queue: 'upload'},
-               solr_queue: 'solr_queue', solr: {}}
+               solr_queue: 'solr_queue', solr: {}, postgres_queue: 'postgres'}
     @upload_worker = UploadWorker.new(@options)
     @upload_worker.connect
     @exchange = @upload_worker.instance_variable_get(:@exchange)
   end
 
-  describe '#add_item' do
+  describe '#create_item_solr' do
 
-    it 'creates a job for the Solr Worker' do
-      allow(@upload_worker).to receive(:expand_json_ld)
+    it 'adds a create item job to the Solr Worker queue' do
+      # allow(@upload_worker).to receive(:expand_json_ld)
       example = {key: 'vaule'}
       allow(@upload_worker).to receive(:create_solr_document).and_return(example)
       expected = '{"action": "add", "document": {"key":"vaule"}}'
       solr_queue = @exchange.get_queue(@options[:solr_queue])
-      @upload_worker.add_item('')
+      @upload_worker.create_item_solr('')
       expect(solr_queue.messages.first).to eq(expected)
+    end
+
+  end
+
+  describe '#create_item_postgres' do
+
+    it 'adds a create item job to the Postgres Worker queue' do
+      # allow(@upload_worker).to receive(:expand_json_ld)
+      example = {key: 'vaule'}
+      allow(@upload_worker).to receive(:create_pg_statement).and_return(example)
+      expected = '{"action": "create", "payload": {"key":"vaule"}}'
+      postgres_queue = @exchange.get_queue(@options[:postgres_queue])
+      @upload_worker.create_item_postgres('')
+      expect(postgres_queue.messages.first).to eq(expected)
     end
 
   end
