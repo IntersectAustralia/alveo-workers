6267e225ae0b1a0c0ee257fed886ba535745fc10
Ilya Anisimoff <ilya@intersect.org.au>
2015-09-28 17:10:29 +1000

    HSCVLAB-MISC: Ingester experiments

---
+++ script/ingester_test.rb
@@ -0,0 +1,155 @@
+$LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib")
+
+require 'yaml'
+require 'bunny'
+require 'rdf/turtle'
+require 'json/ld'
+require 'thread'
+
+class Ingester
+
+  def initialize(options)
+    @input_queue = Queue.new
+    @solr_message_queue = Queue.new
+    @sesame_message_queue = Queue.new
+    @sesame_queue = Queue.new
+    @solr_queue = Queue.new
+    bunny_client_class = Module.const_get(options[:client_class])
+    @bunny_client = bunny_client_class.new(options)
+    @exchange_name = options[:exchange]
+    @upload_queue_name = options[:upload_queue]
+    @sesame_queue_name = options[:sesame_queue]
+    @error_logger = Logger.new(options[:error_log])
+  end
+
+  def connect
+    @bunny_client.start
+  end
+
+  def close
+    @bunny_client.close
+  end
+
+  def process(dir)
+    collection = File.basename(dir)
+    create_input_jobs(dir)
+    process_input(collection)
+    process_solr_message
+    process_sesame_message
+    process_solr
+    process_sesame
+  end
+
+  def process_sesame
+    sesame_worker = Thread.new do
+      channel = @bunny_client.create_channel
+      exchange = channel.direct(@exchange_name)
+      sesame_queue = channel.queue(@sesame_queue_name)
+      sesame_queue.bind(exchange)
+      until (message = @sesame_queue.pop) == :END
+        exchange.publish(message, routing_key: sesame_queue.name)
+      end
+    end
+    sesame_worker.join
+    puts 'process_sesame'
+  end
+
+  def process_solr
+    solr_worker = Thread.new do
+      channel = @bunny_client.create_channel
+      exchange = channel.direct(@exchange_name)
+      upload_queue = channel.queue(@upload_queue_name)
+      upload_queue.bind(exchange)
+      until (message = @solr_queue.pop) == :END
+        exchange.publish(message, routing_key: upload_queue.name)
+      end
+    end
+    solr_worker.join
+    puts 'process_solr'
+  end
+
+  def process_solr_message
+    solr_message_worker = Thread.new do
+      until (ttl_string = @solr_message_queue.pop) == :END
+        graph = RDF::Graph.new
+        RDF::Reader.for(:ttl).new(ttl_string[:payload]) { |reader|
+          reader.each_statement { |statement|
+            graph << statement
+          }
+        }
+        json_ld = graph.dump(:jsonld)
+        #TODO: Move actions to message headers
+        message = "{\"action\": \"add item\", \"metadata\":#{json_ld}}"
+        @solr_queue << message
+      end
+    end
+    solr_message_worker.join
+    @solr_queue << :END
+    puts 'process_solr_message'
+  end
+
+  def process_sesame_message
+    sesame_message_worker = Thread.new do
+      until (turtle = @sesame_message_queue.pop) == :END
+        # turtle = File.open(rdf_file[:payload]).read
+        message = "{\"action\": \"add\",\"collection\": \"#{turtle[:collection]}\", \"payload\": #{turtle[:payload].to_json} }"
+        @sesame_queue << message
+      end
+    end
+    sesame_message_worker.join
+    @sesame_queue << :END
+    puts 'process_sesame_message'
+  end
+
+  def process_input(collection)
+    input_worker = Thread.new do
+      until (file_path = @input_queue.pop) == :END
+        input_job = {collection: collection, payload: File.read(file_path)}
+        if is_metadata? file_path
+          input_job[:type] = 'metadata'
+          @solr_message_queue << input_job
+        else
+          input_job[:type] = 'annotations'
+        end
+        @sesame_message_queue << input_job
+      end
+    end
+    input_worker.join
+    @solr_message_queue << :END
+    @sesame_message_queue << :END
+
+    puts 'process_input'
+  end
+
+  def create_input_jobs(dir)
+    Dir[File.join(dir, '**', '*')].each { |path|
+      if (File.file? path) && (File.extname(path) == '.rdf')
+        @input_queue << path
+      end
+    }
+    @input_queue << :END
+
+    puts 'create_input_jobs'
+  end
+
+  def is_metadata?(file_path)
+    File.basename(file_path, '.rdf').end_with?('metadata')
+  end
+
+end
+
+
+
+def main(directory)
+  config = YAML.load_file("#{File.dirname(__FILE__)}/../spec/files/config.yml")
+  ingester = Ingester.new(config[:ingester])
+  ingester.connect
+  ingester.process(directory)
+  ingester.close
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  # TODO: use an argument parser
+  main(ARGV[0])
+end
--- script/json_remap_test.rb
+++ script/json_remap_test.rb
@@ -37,61 +37,48 @@ def load_document_local(url, options={}, &block)
  loader
 end
 
-def load_document_local(url, options={}, &block)
-  loader = Proc.new{|url, options={}, &block|
-  # alveo_schema = {'@context' => {"commonProperties" => { "@id" => "http://purl.org/dada/schema/0.2#commonProperties" },
-  #                  "dada" => { "@id" => "http://purl.org/dada/schema/0.2#" },
-  #                  "type" => { "@id" => "http://purl.org/dada/schema/0.2#type" },
-  #                  "start" => { "@id" => "http://purl.org/dada/schema/0.2#start" },
-  #                  "end" => { "@id" => "http://purl.org/dada/schema/0.2#end" },
-  #                  "label" => { "@id" => "http://purl.org/dada/schema/0.2#label" },
-  #                  "alveo" => { "@id" => "http://alveo.edu.au/schema/" },
-  #                  "ace" => { "@id" => "http://ns.ausnc.org.au/schemas/ace/" },
-  #                  "ausnc" => { "@id" => "http://ns.ausnc.org.au/schemas/ausnc_md_model/" },
-  #                  "austalk" => { "@id" => "http://ns.austalk.edu.au/" },
-  #                  "austlit" => { "@id" => "http://ns.ausnc.org.au/schemas/austlit/" },
-  #                  "bibo" => { "@id" => "http://purl.org/ontology/bibo/" },
-  #                  "cooee" => { "@id" => "http://ns.ausnc.org.au/schemas/cooee/" },
-  #                  "dc" => { "@id" => "http://purl.org/dc/terms/" },
-  #                  "foaf" => { "@id" => "http://xmlns.com/foaf/0.1/" },
-  #                  "gcsause" => { "@id" => "http://ns.ausnc.org.au/schemas/gcsause/" },
-  #                  "ice" => { "@id" => "http://ns.ausnc.org.au/schemas/ice/" },
-  #                  "olac" => { "@id" => "http://www.language-archives.org/OLAC/1.1/" },
-  #                  "purl" => { "@id" => "http://purl.org/" },
-  #                  "rdf" => { "@id" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#" },
-  #                  "schema" => { "@id" => "http://schema.org/" },
-  #                  "xsd" => { "@id" => "http://www.w3.org/2001/XMLSchema#" }}
-  #  }
-  # if RDF::URI(url, canonicalize: true) == RDF::URI('https://app.alveo.edu.au/schema/json-ld')
-  if false
-    remote_document = JSON::LD::API::RemoteDocument.new(url, alveo_schema)
-    return block_given? ? yield(remote_document) : remote_document
-  else
-    JSON::LD::API.documentLoader(url, options, &block)
-  end
- }
- loader
-end
 
 
 def main
 
-# raw_context = {
-#                 "ausnc:audience" =>{ "@type"=>"@id"},
-#                 "ausnc:communication_setting"=>{ "@type"=>"@id" },
-#                 "ausnc:mode"=>{  "@type"=>"@id" },
-#                 "ausnc:publication_status"=>{  "@type"=>"@id" },
-#                 "ausnc:written_mode"=>{  "@type"=>"@id" },
-#                 "ausnc:itemwordcount"=>{ "@type"=>"xsd:integer"},
-#                 "dcterms:extent"=>{ "@type"=>"xsd:integer"},
-#                 "dcterms:source"=>{  "@type"=>"@id" },
-#                 "hcsvlab" => "http://hcsvlab.org/vocabulary/",
-#                 "hcsvlab:display_document" => {  "@type"=>"@id" },
-#                 "hcsvlab:indexable_document" => {  "@type"=>"@id" },
-#                 "ausnc:document" => {"@type" => "@id"},
-#                 # "ausnc:document" => {"@type" => "foaf:Document"},
-#                 "dc:isPartOf"=>{  "@type"=>"@id" },
-#              }
+raw_context = {
+"commonProperties" =>{"@id" =>"http://purl.org/dada/schema/0.2#commonProperties"},
+"dada" =>{"@id" =>"http://purl.org/dada/schema/0.2#"},
+"type" =>{"@id" =>"http://purl.org/dada/schema/0.2#type"},
+"start" =>{"@id" =>"http://purl.org/dada/schema/0.2#start"},
+"end" =>{"@id" =>"http://purl.org/dada/schema/0.2#end"},
+"label" =>{"@id" =>"http://purl.org/dada/schema/0.2#label"},
+"alveo" =>{"@id" =>"http://alveo.edu.au/schema/"},
+"ace" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ace/"},
+"ausnc" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ausnc_md_model/"},
+"austalk" =>{"@id" =>"http://ns.austalk.edu.au/"},
+"austlit" =>{"@id" =>"http://ns.ausnc.org.au/schemas/austlit/"},
+"bibo" =>{"@id" =>"http://purl.org/ontology/bibo/"},
+"cooee" =>{"@id" =>"http://ns.ausnc.org.au/schemas/cooee/"},
+"dc" =>{"@id" =>"http://purl.org/dc/terms/"},
+"foaf" =>{"@id" =>"http://xmlns.com/foaf/0.1/"},
+"gcsause" =>{"@id" =>"http://ns.ausnc.org.au/schemas/gcsause/"},
+"ice" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ice/"},
+"olac" =>{"@id" =>"http://www.language-archives.org/OLAC/1.1/"},
+"purl" =>{"@id" =>"http://purl.org/"},
+"rdf" =>{"@id" =>"http://www.w3.org/1999/02/22-rdf-syntax-ns#"},
+"schema" =>{"@id" =>"http://schema.org/"},
+"xsd" =>{"@id" =>"http://www.w3.org/2001/XMLSchema#"},
+"ausnc:audience" =>{ "@type"=>"@id"},
+"ausnc:communication_setting"=>{ "@type"=>"@id" },
+"ausnc:mode"=>{  "@type"=>"@id" },
+"ausnc:publication_status"=>{  "@type"=>"@id" },
+"ausnc:written_mode"=>{  "@type"=>"@id" },
+"ausnc:itemwordcount"=>{ "@type"=>"xsd:integer"},
+"dcterms:extent"=>{ "@type"=>"xsd:integer"},
+"dcterms:source"=>{  "@type"=>"@id" },
+"hcsvlab" => "http://hcsvlab.org/vocabulary/",
+"hcsvlab:display_document" => {  "@type"=>"@id" },
+"hcsvlab:indexable_document" => {  "@type"=>"@id" },
+"ausnc:document" => {"@type" => "@id"},
+"ausnc:document" => {"@type" => "foaf:Document"},
+"dc:isPartOf"=>{  "@type"=>"@id" },
+             }
 
   context = ["https://app.alveo.edu.au/schema/json-ld",
               {
@@ -115,9 +102,11 @@ def main
   rdf_file = "#{File.dirname(__FILE__)}/../spec/files/turtle_example.rdf"
   graph = RDF::Graph.load(rdf_file, :format => :ttl)
   jsonld = JSON.parse(graph.dump(:jsonld, context: context))
+  # jsonld = JSON.parse(graph.dump(:jsonld, context: raw_context))
   # json_file = "#{File.dirname(__FILE__)}/../spec/files/json-ld_example_revised.json"
   # graph2 = RDF::Graph.load(json_file, :format => :json_ld)
   # jsonld = JSON.parse(File.open(json_file).read)
+  # compact_with_loader(jsonld, context)
   require 'pry'
   binding.pry
 end
@@ -148,14 +137,15 @@ def compact_with_loader(input, context)
   #  }
   alveo_schema = '{"@context":{"commonProperties":{"@id":"http://purl.org/dada/schema/0.2#commonProperties"},"dada":{"@id":"http://purl.org/dada/schema/0.2#"},"type":{"@id":"http://purl.org/dada/schema/0.2#type"},"start":{"@id":"http://purl.org/dada/schema/0.2#start"},"end":{"@id":"http://purl.org/dada/schema/0.2#end"},"label":{"@id":"http://purl.org/dada/schema/0.2#label"},"alveo":{"@id":"http://alveo.edu.au/schema/"},"ace":{"@id":"http://ns.ausnc.org.au/schemas/ace/"},"ausnc":{"@id":"http://ns.ausnc.org.au/schemas/ausnc_md_model/"},"austalk":{"@id":"http://ns.austalk.edu.au/"},"austlit":{"@id":"http://ns.ausnc.org.au/schemas/austlit/"},"bibo":{"@id":"http://purl.org/ontology/bibo/"},"cooee":{"@id":"http://ns.ausnc.org.au/schemas/cooee/"},"dc":{"@id":"http://purl.org/dc/terms/"},"foaf":{"@id":"http://xmlns.com/foaf/0.1/"},"gcsause":{"@id":"http://ns.ausnc.org.au/schemas/gcsause/"},"ice":{"@id":"http://ns.ausnc.org.au/schemas/ice/"},"olac":{"@id":"http://www.language-archives.org/OLAC/1.1/"},"purl":{"@id":"http://purl.org/"},"rdf":{"@id":"http://www.w3.org/1999/02/22-rdf-syntax-ns#"},"schema":{"@id":"http://schema.org/"},"xsd":{"@id":"http://www.w3.org/2001/XMLSchema#"}}}'
   loader = Proc.new{|url, options={}, &block|
-    remote_document = nil
     if RDF::URI(url, canonicalize: true) == RDF::URI('https://app.alveo.edu.au/schema/json-ld')
-      remote_document = JSON::LD::API::RemoteDocument.new(url, alveo_schema)
-      if block_given?
-        yield(remote_document)
-      else
-        return remote_document
-      end
+      # remote_document = JSON::LD::API::RemoteDocument.new(url, alveo_schema)
+      remote_document = JSON::LD::API::RemoteDocument.new(url, File.read("#{File.dirname(__FILE__)}/../spec/files/alveo.json"))
+      # if block_given?
+      #   yield(remote_document)
+      # else
+      #    remote_document
+      # end
+      block_given? ? yield(remote_document) : remote_document
     else
       JSON::LD::API.documentLoader(url, options, &block)
     end
@@ -168,7 +158,7 @@ def load_doc(url, options={}, &block)
   alveo_schema = '{"@context":{"commonProperties":{"@id":"http://purl.org/dada/schema/0.2#commonProperties"},"dada":{"@id":"http://purl.org/dada/schema/0.2#"},"type":{"@id":"http://purl.org/dada/schema/0.2#type"},"start":{"@id":"http://purl.org/dada/schema/0.2#start"},"end":{"@id":"http://purl.org/dada/schema/0.2#end"},"label":{"@id":"http://purl.org/dada/schema/0.2#label"},"alveo":{"@id":"http://alveo.edu.au/schema/"},"ace":{"@id":"http://ns.ausnc.org.au/schemas/ace/"},"ausnc":{"@id":"http://ns.ausnc.org.au/schemas/ausnc_md_model/"},"austalk":{"@id":"http://ns.austalk.edu.au/"},"austlit":{"@id":"http://ns.ausnc.org.au/schemas/austlit/"},"bibo":{"@id":"http://purl.org/ontology/bibo/"},"cooee":{"@id":"http://ns.ausnc.org.au/schemas/cooee/"},"dc":{"@id":"http://purl.org/dc/terms/"},"foaf":{"@id":"http://xmlns.com/foaf/0.1/"},"gcsause":{"@id":"http://ns.ausnc.org.au/schemas/gcsause/"},"ice":{"@id":"http://ns.ausnc.org.au/schemas/ice/"},"olac":{"@id":"http://www.language-archives.org/OLAC/1.1/"},"purl":{"@id":"http://purl.org/"},"rdf":{"@id":"http://www.w3.org/1999/02/22-rdf-syntax-ns#"},"schema":{"@id":"http://schema.org/"},"xsd":{"@id":"http://www.w3.org/2001/XMLSchema#"}}}'
   remote_document = nil
   if RDF::URI(url, canonicalize: true) == RDF::URI('https://app.alveo.edu.au/schema/json-ld')
-    remote_document = JSON::LD::API::RemoteDocument.new(url, alveo_schema)
+    remote_document = JSON::LD::API::RemoteDocument.new(url, File.read("#{File.dirname(__FILE__)}/../spec/files/alveo.json"))
     if block_given?
       yield(remote_document)
     else
