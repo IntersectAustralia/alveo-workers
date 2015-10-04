b5f78d8a38af2fa38a70008779c96d1c8fe15397
Ilya Anisimoff <anisimoff@gmail.com>
2015-10-04 20:57:22 +1100

    HCSVLAB-MISC: Adds test Trove chunk ingester

---
+++ lib/trove_ingester.rb
@@ -0,0 +1,97 @@
+require 'bunny'
+
+class TroveIngester
+
+  def initialize(options)
+    bunny_client_class = Module.const_get(options[:client_class])
+    @bunny_client = bunny_client_class.new(options)
+    @exchange_name = options[:exchange]
+    @error_logger = Logger.new(options[:error_log])
+    @upload_queue_name = options[:upload_queue]
+  end
+
+  def connect
+    @bunny_client.start
+    @channel = @bunny_client.create_channel
+    @exchange = @channel.direct(@exchange_name)
+    @upload_queue = add_queue(@upload_queue_name)
+  end
+
+  def close
+    @bunny_client.close
+  end
+
+  def add_queue(name)
+    queue = @channel.queue(name)
+    queue.bind(@exchange, routing_key: name)
+    queue
+  end
+
+  def self.get_rdf_file_paths(dir)
+    Dir[File.join(dir, '**', '*')].keep_if { |path|
+      (File.file? path) && (File.extname(path) == '.rdf')
+    }
+  end
+
+  def process_chunk(trove_chunk)
+    File.open(trove_chunk, 'r').each { |trove_record|
+      begin
+        trove_fields = JSON.parse(trove_record)
+        message = map_to_json_ld(trove_fields)
+        @exchange.publish(message, routing_key: @upload_queue.name)
+      rescue Exception => e
+        # TODO: Error queue instead of log file
+        @error_logger.error "#{e.class}: #{e.to_s}"
+      end
+    }
+  end
+
+  def map_to_json_ld(trove_fields)
+    %Q(
+      {
+        "@context": ["https://app.alveo.edu.au/schema/json-ld",
+          {
+            "ausnc:audience" : { "@type": "@id"},
+            "ausnc:communication_setting": { "@type": "@id" },
+            "ausnc:mode": {  "@type": "@id" },
+            "ausnc:publication_status": {  "@type": "@id" },
+            "ausnc:written_mode": {  "@type": "@id" },
+            "ausnc:itemwordcount": { "@type": "xsd:integer"},
+            "dcterms:extent": { "@type": "xsd:integer"},
+            "dcterms:source": {  "@type": "@id" }
+         }],
+        "@graph": {
+          "@id": "#{trove_fields['@id']}", # TODO: Generated, this should probably be mapped to dc:identifier
+          "@type": "ausnc:AusNCObject",
+          "ausnc:audience": "ausnc:mass_market",
+          "ausnc:communication_medium": "ausnc:newspaper",
+          "ausnc:communication_setting": "ausnc:popular",
+          "ausnc:itemwordcount": "#{trove_fields['wordCount']}",
+          "ausnc:mode": "ausnc:written",
+          "ausnc:publication_status": "ausnc:published",
+          "ausnc:state": "#{trove_fields['state']}",
+          "ausnc:written_mode": "ausnc:print",
+          "dc:date": "#{trove_fields['date']}",
+          "dc:identifier": "#{trove_fields['id']}",
+          "dc:source": "#{trove_fields['titleName']}",
+          "dc:title": "#{trove_fields['heading']}",
+          "fulltext": "#{trove_fields['fulltext']}", # TODO: Perhaps this fields goes outside the graph?
+          "trove:category": "#{trove_fields['category']}",
+          "trove:firstPageId": "#{trove_fields['firstPageId']}",
+          "trove:firstPageSeq": "#{trove_fields['firstPageSeq']}",
+          "olac:language": "eng",
+          "ausnc:document": [
+            {
+              "@type": "foaf:Document",
+              "dcterms:extent": "#{trove_fields['fulltext'].size}",
+              "dcterms:identifier": "", TODO: Generated
+              "dcterms:source": "", TODO: Generated, linking to Steve's index
+              "dcterms:type": "Text"
+            },
+        }
+      }
+    )
+  end
+
+
+end
--- script/test.rb
+++ script/test.rb
@@ -6,56 +6,63 @@ require 'active_record'
 require 'models/item'
 require 'models/document'
 require 'postgres_helper'
+require 'trove_ingester'
 
 def main(config)
-  sesame_client = SesameClient.new(config[:sesame_worker])
-  turtle = File.open("#{File.dirname(__FILE__)}/../spec/files/turtle_example.rdf").read
-  options = {adapter: 'postgresql', database: 'hcsvlab', user: 'hcsvlab', host: 'localhost'}
-  ActiveRecord::Base.establish_connection(options)
-  json_ld_expanded = JSON.parse(File.read("#{File.dirname(__FILE__)}/../spec/files/json-ld_expanded_example.json"))
+  # sesame_client = SesameClient.new(config[:sesame_worker])
+  # turtle = File.open("#{File.dirname(__FILE__)}/../spec/files/turtle_example.rdf").read
+  # options = {adapter: 'postgresql', database: 'hcsvlab', user: 'hcsvlab', host: 'localhost'}
+  # ActiveRecord::Base.establish_connection(options)
+  # json_ld_expanded = JSON.parse(File.read("#{File.dirname(__FILE__)}/../spec/files/json-ld_expanded_example.json"))
 
-  context = {
-      "ace" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ace/"},
-      "alveo" =>{"@id" =>"http://alveo.edu.au/schema/"},
-      "ausnc" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ausnc_md_model/"},
-      "austalk" =>{"@id" =>"http://ns.austalk.edu.au/"},
-      "austlit" =>{"@id" =>"http://ns.ausnc.org.au/schemas/austlit/"},
-      "bibo" =>{"@id" =>"http://purl.org/ontology/bibo/"},
-      "commonProperties" =>{"@id" =>"http://purl.org/dada/schema/0.2#commonProperties"},
-      "cooee" =>{"@id" =>"http://ns.ausnc.org.au/schemas/cooee/"},
-      "dada" =>{"@id" =>"http://purl.org/dada/schema/0.2#"},
-      "dc" =>{"@id" =>"http://purl.org/dc/terms/"},
-      "end" =>{"@id" =>"http://purl.org/dada/schema/0.2#end"},
-      "foaf" =>{"@id" =>"http://xmlns.com/foaf/0.1/"},
-      "gcsause" =>{"@id" =>"http://ns.ausnc.org.au/schemas/gcsause/"},
-      "ice" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ice/"},
-      "label" =>{"@id" =>"http://purl.org/dada/schema/0.2#label"},
-      "olac" =>{"@id" =>"http://www.language-archives.org/OLAC/1.1/"},
-      "purl" =>{"@id" =>"http://purl.org/"},
-      "rdf" =>{"@id" =>"http://www.w3.org/1999/02/22-rdf-syntax-ns#"},
-      "schema" =>{"@id" =>"http://schema.org/"},
-      "start" =>{"@id" =>"http://purl.org/dada/schema/0.2#start"},
-      "type" =>{"@id" =>"http://purl.org/dada/schema/0.2#type"},
-      "xsd" =>{"@id" =>"http://www.w3.org/2001/XMLSchema#"},
-      # "hcsvlab" => "http://hcsvlab.org/vocabulary/",
-      "hcsvlab" => {"@id" => "http://hcsvlab.org/vocabulary/"},
-      "ausnc:audience" =>{ "@type"=>"@id"},
-      "ausnc:communication_setting"=>{ "@type"=>"@id" },
-      "ausnc:document" => {"@type" => "@id"},
-      # "ausnc:itemwordcount"=>{ "@type"=>"xsd:integer"},
-      # "ausnc:itemwordcount"=>{ "@type"=>"@id"},
-      "ausnc:mode"=>{  "@type"=>"@id" },
-      "ausnc:publication_status"=>{  "@type"=>"@id" },
-      "ausnc:written_mode"=>{  "@type"=>"@id" },
-      "dc:isPartOf"=>{  "@type"=>"@id" },
-      "dcterms:extent"=>{ "@type"=>"xsd:integer"},
-      "dcterms:source"=>{  "@type"=>"@id" },
-      "hcsvlab:display_document" => {  "@type"=>"@id" },
-      "hcsvlab:indexable_document" => {  "@type"=>"@id" }
-      }.freeze
+  # context = {
+  #     "ace" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ace/"},
+  #     "alveo" =>{"@id" =>"http://alveo.edu.au/schema/"},
+  #     "ausnc" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ausnc_md_model/"},
+  #     "austalk" =>{"@id" =>"http://ns.austalk.edu.au/"},
+  #     "austlit" =>{"@id" =>"http://ns.ausnc.org.au/schemas/austlit/"},
+  #     "bibo" =>{"@id" =>"http://purl.org/ontology/bibo/"},
+  #     "commonProperties" =>{"@id" =>"http://purl.org/dada/schema/0.2#commonProperties"},
+  #     "cooee" =>{"@id" =>"http://ns.ausnc.org.au/schemas/cooee/"},
+  #     "dada" =>{"@id" =>"http://purl.org/dada/schema/0.2#"},
+  #     "dc" =>{"@id" =>"http://purl.org/dc/terms/"},
+  #     "end" =>{"@id" =>"http://purl.org/dada/schema/0.2#end"},
+  #     "foaf" =>{"@id" =>"http://xmlns.com/foaf/0.1/"},
+  #     "gcsause" =>{"@id" =>"http://ns.ausnc.org.au/schemas/gcsause/"},
+  #     "ice" =>{"@id" =>"http://ns.ausnc.org.au/schemas/ice/"},
+  #     "label" =>{"@id" =>"http://purl.org/dada/schema/0.2#label"},
+  #     "olac" =>{"@id" =>"http://www.language-archives.org/OLAC/1.1/"},
+  #     "purl" =>{"@id" =>"http://purl.org/"},
+  #     "rdf" =>{"@id" =>"http://www.w3.org/1999/02/22-rdf-syntax-ns#"},
+  #     "schema" =>{"@id" =>"http://schema.org/"},
+  #     "start" =>{"@id" =>"http://purl.org/dada/schema/0.2#start"},
+  #     "type" =>{"@id" =>"http://purl.org/dada/schema/0.2#type"},
+  #     "xsd" =>{"@id" =>"http://www.w3.org/2001/XMLSchema#"},
+  #     # "hcsvlab" => "http://hcsvlab.org/vocabulary/",
+  #     "hcsvlab" => {"@id" => "http://hcsvlab.org/vocabulary/"},
+  #     "ausnc:audience" =>{ "@type"=>"@id"},
+  #     "ausnc:communication_setting"=>{ "@type"=>"@id" },
+  #     "ausnc:document" => {"@type" => "@id"},
+  #     # "ausnc:itemwordcount"=>{ "@type"=>"xsd:integer"},
+  #     # "ausnc:itemwordcount"=>{ "@type"=>"@id"},
+  #     "ausnc:mode"=>{  "@type"=>"@id" },
+  #     "ausnc:publication_status"=>{  "@type"=>"@id" },
+  #     "ausnc:written_mode"=>{  "@type"=>"@id" },
+  #     "dc:isPartOf"=>{  "@type"=>"@id" },
+  #     "dcterms:extent"=>{ "@type"=>"xsd:integer"},
+  #     "dcterms:source"=>{  "@type"=>"@id" },
+  #     "hcsvlab:display_document" => {  "@type"=>"@id" },
+  #     "hcsvlab:indexable_document" => {  "@type"=>"@id" }
+  #     }.freeze
 
-  item = json_ld_expanded[0]
-  compacted = JSON::LD::API.compact(item, context)
+  # item = json_ld_expanded[0]
+  # compacted = JSON::LD::API.compact(item, context)
+
+  ingester = TroveIngester.new(config[:ingester])
+  trove_chunk = "#{File.dirname(__FILE__)}/../spec/files/data-1.dat"
+  ingester.connect
+  trove_example = JSON.parse(File.read("#{File.dirname(__FILE__)}/../spec/files/trove_example.json"))
+
   require 'pry'
   binding.pry
 end
