f83902f59955470874ca10bcf68bee185505459f
Ilya Anisimoff <ilya@intersect.org.au>
2016-01-27 15:30:56 +1100

    HCSVLAB-MISC: Removes unused files

--- scratch.rb
+++
@@ -1,8 +0,0 @@
-
-SESAME_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/sesame.yml")[Rails.env]
-server = RDF::Sesame::HcsvlabServer.new(SESAME_CONFIG["url"].to_s)
-repository = server.repository('ace')
-rdf_uri = RDF::URI.new('http://ns.ausnc.org.au/corpora/ace/items/E29a')
-basic_results = repository.query(:subject => rdf_uri)
-
-document_predicate = RDF::URI.new('http://ns.austalk.edu.au/document')
