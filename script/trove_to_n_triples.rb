be5b27ad1dac8e826bd0c9bde618d4ce524470d4
Ilya Anisimoff <ilya@intersect.org.au>
2016-01-27 15:25:23 +1100

    HCSVLAB-MISC: Modifies conversion RDF script

--- script/trove_to_n_triples.rb
+++ script/trove_to_n_triples.rb
@@ -3,9 +3,10 @@ $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib")
 require 'yaml'
 require 'trove_ingester'
 require 'metadata_helper'
+require 'sesame_helper'
 
 def main(config)
-  trove_chunk = "/Users/ilya/workspace/corpora/trove/data-1.dat"
+  trove_chunk = "/Users/ilya/workspace/corpora/trove/data-3.dat"
   process_chunk(trove_chunk, config)
 end
 
@@ -14,9 +15,11 @@ end
 def process_chunk(trove_chunk, config)
   ingester = TroveIngester.new(config[:ingester])
   metadata_helper = Class.new.include(MetadataHelper).new
-  outfile = File.open('n3_chunk.dat', 'a')
+  outfile = File.open('n3_chunk3.dat', 'a')
   File.open(trove_chunk, 'r:iso-8859-1').each { |trove_record|
     begin
+      # require 'pry'
+      # binding.pry
       trove_fields = JSON.parse(trove_record.encode('utf-8'))
       trove_message = ingester.map_to_json_ld(trove_fields)
       trove_item = JSON.parse(trove_message)['items'].first
@@ -24,6 +27,7 @@ def process_chunk(trove_chunk, config)
       n3_string = json_ld_to_n3(trove_item)
       outfile.write(n3_string)
     rescue Exception => e
+      p e
     end
   }
   outfile.close
